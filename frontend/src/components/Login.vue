c8d8b602e2e9f0d3588fd04e44d5e223551fdb4a
Sebastian Montoya Zuluaga <sebastian.montoyaz@perficient.com>
2023-04-26 15:54:17 -0500

    test

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -7,7 +7,7 @@
                 <div class='row'>
                     <div class='col-md-3'></div>
                     <div class='col-md-6'>
-                        <h2>Please Login you fucker</h2>
+                        <h2>Please Login you sonofabitch</h2>
                         <hr>
                     </div>
                 </div>
