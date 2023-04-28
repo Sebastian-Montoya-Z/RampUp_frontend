1a13f5c573000b580ad676bfa1afd411b0803307
Sebastian Montoya Zuluaga <sebastian.montoyaz@perficient.com>
2023-04-28 18:39:53 -0500

    fix

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -7,7 +7,7 @@
                 <div class='row'>
                     <div class='col-md-3'></div>
                     <div class='col-md-6'>
-                        <h2>Please Login bitch</h2>
+                        <h2>Please Login dear loving user</h2>
                         <hr>
                     </div>
                 </div>
